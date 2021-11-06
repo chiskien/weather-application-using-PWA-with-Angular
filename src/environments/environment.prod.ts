8c580b8005da019c3265e89e381f1e0dcc8065ee
ChisKien <chiskien03214@gmail.com>
2021-11-06 10:17:20 +0700

    Add api endpoint and api key to enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: 'de4ec913e35ac37250373570a53c0fb6',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: 'de4ec913e35ac37250373570a53c0fb6',
 };
 
 /*
