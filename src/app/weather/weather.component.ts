4176167e0a3230e2c617107271c4edf40714ee8d
ChisKien <chiskien03214@gmail.com>
2021-11-06 11:22:24 +0700

    implement weather component

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Weather } from '../weather';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
 })
 export class WeatherComponent implements OnInit {
+  weather: Weather | undefined;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {}
 
-  ngOnInit(): void {
+  search(city: string) {
+    this.weatherService
+      .getWeather(city)
+      .subscribe((response) => (this.weather = response));
   }
-
+  ngOnInit(): void {}
 }
